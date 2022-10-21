db2cc4ad05202fb56ff156f3732b51497fbce5f8
feryeong <feryeong@nc-and.com>
2022-10-21 15:02:57 +0900

    i++

--- src/views/RTC.js
+++ src/views/RTC.js
@@ -65,7 +65,9 @@ async function getMediaStream(deviceId = {}){
 }
 function getStringSize(str){
   return (function(str, b, i, c){
-    for(b = i = 0; c = str.charCodeAt(i++) ;b+=c >> 11 ? 3 : c >> 7 ? 2 : 1){};
+    for(b = i = 0; c = str.charCodeAt(i) ;b+=c >> 11 ? 3 : c >> 7 ? 2 : 1){
+      i++
+    };
     return b
   }(str))
 }
